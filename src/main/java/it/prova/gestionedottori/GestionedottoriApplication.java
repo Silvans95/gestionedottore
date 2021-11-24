af8d036868cc7e0766a1364ae5a4332c9ddce8ef
silvans95 <silvano.paloni@gmail.com>
2021-11-24 15:00:08 +0100

    bugfix

--- src/main/java/it/prova/gestionedottori/GestionedottoriApplication.java
+++ src/main/java/it/prova/gestionedottori/GestionedottoriApplication.java
@@ -21,14 +21,14 @@ public class GestionedottoriApplication {
 		return (args) -> {
 
 			// inizializzo il Db
-			dottoreService.save(new Dottore("nome1", "cognome1", "codice1", true, false));
-			dottoreService.save(new Dottore("nome2", "cognome2", "codice2", true, false));
-			dottoreService.save(new Dottore("nome3", "cognome3", "codice3", true, false));
-			dottoreService.save(new Dottore("nome4", "cognome4", "codice4", true, false));
-			dottoreService.save(new Dottore("nome5", "cognome5", "codice5", true, false));
-			dottoreService.save(new Dottore("nome6", "cognome6", "codice6", true, false));
-			dottoreService.save(new Dottore("nome7", "cognome7", "codice7", true, false));
-			dottoreService.save(new Dottore("nome8", "cognome8", "codice8", true, false));
+			dottoreService.save(new Dottore("nome1", "cognome1", "codice1", false, true));
+			dottoreService.save(new Dottore("nome2", "cognome2", "codice2", false, true));
+			dottoreService.save(new Dottore("nome3", "cognome3", "codice3", false, true));
+			dottoreService.save(new Dottore("nome4", "cognome4", "codice4", false, true));
+			dottoreService.save(new Dottore("nome5", "cognome5", "codice5", false, true));
+			dottoreService.save(new Dottore("nome6", "cognome6", "codice6", false, true));
+			dottoreService.save(new Dottore("nome7", "cognome7", "codice7", false, true));
+			dottoreService.save(new Dottore("nome8", "cognome8", "codice8", false, true));
 			
 			// verifico inserimento
 			System.out.println("Elenco Dottori");
